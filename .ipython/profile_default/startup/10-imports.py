2f9af0c25456a5d35d65695416bb0715b766e37a
sugarraysam <samuel.blaisdowdy@protonmail.com>
2020-04-22 17:23:34 -0400

    started assignment1 week1 applied-machine-learning

--- .ipython/profile_default/startup/10-imports.py
+++ .ipython/profile_default/startup/10-imports.py
@@ -1,8 +1,4 @@
 import numpy as np
 import pandas as pd
-
-# import PyQt5
-# import matplotlib
-# matplotlib.use('qt5')
-# import matplotlib.pyplot as plt
-# import seaborn as sns
+import matplotlib.pyplot as plt
+import seaborn as sns
